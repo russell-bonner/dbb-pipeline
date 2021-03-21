a0f16322f38037993e78c8d7a7ae8b9c96536783
Russell Bonner <russell.bonner@uk.ibm.com>
2021-03-21 20:35:06 +0000

    06

--- cics-genapp/base/src/COBOL/LGICUS01.cbl
+++ cics-genapp/base/src/COBOL/LGICUS01.cbl
@@ -19,7 +19,7 @@
       *                                                                *
       * Customer Inquire Business logic                                *
       *                                                                *
-      *  05                                                            *
+      *  06                                                            *
       *                                                                *
       ******************************************************************
        IDENTIFICATION DIVISION.
